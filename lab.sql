95b2aac0be5eddb9a73802f5e1131fe8dcf3ab7b
Jack Ball <ball.jack2376@gmail.com>
2023-08-29 13:12:48 -0600

    init commit

---
+++ lab.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students (
+    github VARCHAR(30) PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30)
+);
+
+INSERT INTO students VALUES ('jhacks', 'jane', 'hacker');
+INSERT INTO students VALUES ('sdevelops', 'sarah', 'developer')
+
+DROP TABLE students;
+
